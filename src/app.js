21069d0b62cad79aafe8d68ab977f45f74d4bb86
jorgepardor <jorgeluis.mail@gmail.com>
2022-01-25 16:00:24 +0000

    He añadido un paso mas al ejercicio, ahora construye los arrays para generar los dominios a partir de un random a unos arrays con muchas mas variables. También hice que escribiera en el index los resultados y lo puse bonito :)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,114 @@
-/* eslint-disable */
 import "bootstrap";
+// import { formatWithCursor } from "prettier";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let firstPart = [
+    "the",
+    "our",
+    "your",
+    "their",
+    "one",
+    "some",
+    "that",
+    "this",
+    "many"
+  ];
+
+  let secondPart = [
+    "bitter",
+    "glutenfree",
+    "nontransgenic",
+    "8bit",
+    "tattooed",
+    "sustainable",
+    "nextlevel",
+    "authentic",
+    "raw",
+    "umami",
+    "nongmo",
+    "activated",
+    "tumeric",
+    "vegan"
+  ];
+
+  let thirdPart = [
+    "guru",
+    "skateboard",
+    "shaman",
+    "vinegar",
+    "hoodie",
+    "ethical",
+    "foodtruck",
+    "helvetica",
+    "succulents",
+    "biodiesel",
+    "toast",
+    "distillery",
+    "chicharrones"
+  ];
+
+  let fourthPart = [
+    ".com",
+    ".barefoot",
+    ".beer",
+    ".ads",
+    ".adult",
+    ".bargains",
+    ".boots",
+    ".chat",
+    ".cooking",
+    ".dating",
+    ".dentist"
+  ];
+
+  let pronoun = [];
+  let adj = [];
+  let noun = [];
+  let domains = [];
+
+  function getRandom(array, newArray) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    newArray.push(array[randomIndex]);
+  }
+
+  for (let i = 0; i < 2; i++) {
+    getRandom(firstPart, pronoun);
+    getRandom(secondPart, adj);
+    getRandom(thirdPart, noun);
+    getRandom(fourthPart, domains);
+  }
+
+  function getName() {
+    let resultado = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      let who = pronoun[i];
+
+      for (let j = 0; j < adj.length; j++) {
+        let how = adj[j];
+
+        for (let k = 0; k < noun.length; k++) {
+          let what = noun[k];
+
+          for (let l = 0; l < domains.length; l++) {
+            let dom = domains[l];
+            resultado.push(who + how + what + dom);
+          }
+        }
+      }
+    }
+
+    return resultado;
+  }
+
+  let resultado = getName();
+
+  const half = Math.ceil(resultado.length / 2);
+
+  var firstHalf = resultado.slice(0, half);
+  var secondHalf = resultado.slice(half, resultado.length);
+
+  document.querySelector("#hipsterName").innerHTML = firstHalf.join("<br>");
+  document.querySelector("#hipsterName2").innerHTML = secondHalf.join("<br>");
 };
